5e5fa779231398360417923cbb09ca74334d33c2
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-12 15:12:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+ function getFormvalue() {
     //Write your code here
+	 const form = document.getElementById("form1");
+	 const firstName = form.elements["f-name"].value;
+  const lastName = form.elements["l-name"].value;
 
-}
+  // Display the values in an alert
+  alert(firstName +" "+ lastName);
+ }
