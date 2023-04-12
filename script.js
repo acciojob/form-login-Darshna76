06e93ab5cf67fd55499d50c1e5fdd7174b78d917
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-12 15:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,5 @@
 	 const form = document.getElementById("form1");
 	 const firstName = form.elements["f-name"].value;
   const lastName = form.elements["l-name"].value;
-
-  // Display the values in an alert
   alert(firstName +" "+ lastName);
  }
